e9a8f45dc9ae247169920bc25f7933bb18b17f00
ruch0401 <ruch0401@gmail.com>
2021-08-26 12:09:43 -0700

    removing slider for technical skills display temporarily to allow all other features to work

--- website3/scripts/main.js
+++ website3/scripts/main.js
@@ -22,7 +22,8 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("card");
+  // var x = document.getElementsByClassName("card");
+  var x = document.getElementsByName("skills-card");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
