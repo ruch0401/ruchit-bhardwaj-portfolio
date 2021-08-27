dd3339c3d75771c00e611236c920ab6ae2744a9c
ruch0401 <ruch0401@gmail.com>
2021-08-27 10:28:08 -0700

    added carousel for technical skills and updated the references section

--- website3/scripts/main.js
+++ website3/scripts/main.js
@@ -13,24 +13,24 @@ function calculateAge(dob) {
 }
 
 // function for forward and reverse slider
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-  var i;
-  // var x = document.getElementsByClassName("card");
-  var x = document.getElementsByName("skills-card");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";
-}
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("card");
+//   // var x = document.getElementsByName("skills-card");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
 
 
 
