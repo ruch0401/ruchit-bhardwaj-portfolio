2294da503f8e013698e92ff5fa2fd2a6bf2845ed
ruch0401 <ruch0401@gmail.com>
2022-01-21 00:21:18 -0800

    Updated age calculate method

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -2,64 +2,70 @@
 // Don't forget to add it into respective layouts where this js file is needed
 
 // function to calculate my age dynamically
-$(document).ready(function() {
-  document.getElementById("calculated-date").innerHTML = calculateAge(new Date(1997, 1, 4));
+$(document).ready(function () {
+  document.getElementById("calculated-date").innerHTML = getAge("1997/01/04");
 });
 
-function calculateAge(dob) {
-  var diff_ms = Date.now() - dob.getTime();
-  var age_dt = new Date(diff_ms); 
-  return Math.abs(age_dt.getUTCFullYear() - 1970);
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
 
 function sendEmail() {
   var name = document.getElementById("name").value;
   var to = "ruchitbh@usc.edu";
   var subject = document.getElementById("Subject").value;
-  var subjectWithName =  `${name} | ${subject}`;
+  var subjectWithName = `${name} | ${subject}`;
   var message = document.getElementById("message").value;
   window.open(`mailto:${to}?&subject=${subjectWithName}&body=${message}`);
 }
 
-
-
-$(document).ready(function() {
-  AOS.init( {
+$(document).ready(function () {
+  AOS.init({
     // uncomment below for on-scroll animations to played only once
-    // once: true  
+    // once: true
   }); // initialize animate on scroll library
 });
 
 // Smooth scroll for links with hashes
-$('a.smooth-scroll')
-.click(function(event) {
+$("a.smooth-scroll").click(function (event) {
   // On-page links
   if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
     location.hostname == this.hostname
   ) {
     // Figure out element to scroll to
     var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
     // Does a scroll target exist?
     if (target.length) {
       // Only prevent default if animation is actually gonna happen
       event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000,
+        function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
     }
   }
 });
